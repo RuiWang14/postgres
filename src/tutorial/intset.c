541fa58376dda9647d269994555113f73cbd6f81
ruiwang14 <sprucewang@163.com>
2021-03-13 20:29:03 +0800

    add xor operator

--- src/tutorial/intset.c
+++ src/tutorial/intset.c
@@ -551,3 +551,61 @@ Datum
 
 	PG_RETURN_POINTER(set);
 }
+
+/*
+ * xor
+ * A XOR B 
+ */
+PG_FUNCTION_INFO_V1(intset_xor);
+
+Datum
+	intset_xor(PG_FUNCTION_ARGS)
+{
+	IntSet *setA = (IntSet *)PG_GETARG_POINTER(0);
+	IntSet *setB = (IntSet *)PG_GETARG_POINTER(1);
+
+	int maxSize = setA->size + setB->size;
+
+	int *list = palloc(sizeof(int32) * maxSize);
+	int len = 0;
+
+	for (int i = 0; i < setA->size; i++)
+	{
+		bool find = false;
+		for (int j = 0; j < setB->size; j++)
+		{
+			if (setA->data[i] == setB->data[j])
+			{
+				find = true;
+				break;
+			}
+		}
+		if (find == false)
+		{
+			list[len++] = setA->data[i];
+		}
+	}
+
+	for (int i = 0; i < setB->size; i++)
+	{
+		bool find = false;
+		for (int j = 0; j < setA->size; j++)
+		{
+			if (setB->data[i] == setA->data[j])
+			{
+				find = true;
+				break;
+			}
+		}
+		if (find == false)
+		{
+			list[len++] = setB->data[i];
+		}
+	}
+
+	IntSet *set = newIntSet(len);
+	memcpy(set->data, list, sizeof(int32) * len);
+	pfree(list);
+
+	PG_RETURN_POINTER(set);
+}
